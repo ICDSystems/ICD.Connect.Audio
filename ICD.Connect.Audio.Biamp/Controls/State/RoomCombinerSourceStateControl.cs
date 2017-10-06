af459b69f3f4f0fec9aed48d78b734a94c7897b0
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-06 13:10:54 -0400

    Initial commit of RoomCombinerSourceStateControl

---
+++ ICD.Connect.Audio.Biamp/Controls/State/RoomCombinerSourceStateControl.cs
@@ -0,0 +1,18 @@
+﻿using ICD.Connect.Audio.Biamp.AttributeInterfaces;
+
+namespace ICD.Connect.Audio.Biamp.Controls.State
+{
+	public sealed class RoomCombinerSourceStateControl : AbstractBiampTesiraStateDeviceControl
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="name"></param>
+		/// <param name="stateAttribute"></param>
+		public RoomCombinerSourceStateControl(int id, string name, IStateAttributeInterface stateAttribute)
+			: base(id, name, stateAttribute)
+		{
+		}
+	}
+}
