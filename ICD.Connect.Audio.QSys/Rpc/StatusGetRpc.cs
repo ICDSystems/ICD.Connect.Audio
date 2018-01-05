7f44b2d3cedd6cf4b99f366be1dcc06bf01a2beb
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-05 14:36:52 -0500

    Initial commit of StatusGetRpc

---
+++ ICD.Connect.Audio.QSys/Rpc/StatusGetRpc.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace ICD.Connect.Audio.QSys.Rpc
+{
+	public sealed class StatusGetRpc : AbstractRpc
+	{
+		private const string METHOD_VALUE = "StatusGet";
+
+		public override string Method { get { return METHOD_VALUE; } }
+
+		/// <summary>
+		/// Override to add serialize params to JSON.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void SerializeParams(JsonWriter writer)
+		{
+			writer.WriteValue(0);
+		}
+	}
+}
