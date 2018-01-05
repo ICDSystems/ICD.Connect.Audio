bedeb68d3478c66c8fa49779b131fe52af18fc8e
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-05 10:31:27 -0500

    Initial commit of QSys simplsharp .csproj

---
+++ ICD.Connect.Audio.QSys/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Audio.QSys")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Audio.QSys")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
