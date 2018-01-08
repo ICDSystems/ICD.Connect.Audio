3c3ab53bb38cd0bfeb8622f56d141ad996efbdc1
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-08 11:16:19 -0500

    Component.GetComponents RPC

---
+++ ICD.Connect.Audio.QSys/Rpc/ComponentGetComponentsRpc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ICD.Connect.Audio.QSys.Rpc
+{
+	/// <summary>
+	/// {
+	///		"jsonrpc": "2.0",
+	///		"method": "Component.GetComponents",
+	///		"params": {},
+	///		"id": 1234
+	/// }
+	/// </summary>
+	public sealed class ComponentGetComponentsRpc : AbstractRpc
+	{
+		private const string METHOD_VALUE = "Component.GetComponents";
+
+		public override string Method { get { return METHOD_VALUE; } }
+
+		/// <summary>
+		/// Override to add serialize params to JSON.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void SerializeParams(JsonWriter writer)
+		{
+			if (writer == null)
+				throw new ArgumentNullException("writer");
+
+			writer.WriteStartObject();
+			writer.WriteEndObject();
+		}
+	}
+}
