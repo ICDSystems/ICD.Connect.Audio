d373fd665d0ef4683005d310ad5152191230b51f
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-25 15:15:34 -0500

    Fixing namespaces

--- ICD.Connect.Audio.QSys/QSysCoreDevice.cs
+++ ICD.Connect.Audio.QSys/QSysCoreDevice.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using ICD.Common.Properties;
-using ICD.Common.Services.Logging;
 using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Json;
+using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Devices;
 using ICD.Connect.Protocol.Extensions;
