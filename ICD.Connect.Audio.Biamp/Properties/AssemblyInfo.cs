3eb0cf0f902914917b337804c248a7bdde947af7
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-07 16:16:42 -0500

    Removing wildcard versioning

--- ICD.Connect.Audio.Biamp/Properties/AssemblyInfo.cs
+++ ICD.Connect.Audio.Biamp/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Audio.Biamp")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
