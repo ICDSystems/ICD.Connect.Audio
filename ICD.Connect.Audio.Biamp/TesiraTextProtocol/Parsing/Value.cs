c657236937305bee1031fbb28cff314c0a5cd1b6
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-27 10:10:36 -0400

    feat: Method for mapping a TTP serial to a C# object with a default value

--- ICD.Connect.Audio.Biamp/TesiraTextProtocol/Parsing/Value.cs
+++ ICD.Connect.Audio.Biamp/TesiraTextProtocol/Parsing/Value.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using ICD.Common.Properties;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
 
@@ -216,7 +215,7 @@ namespace ICD.Connect.Audio.Biamp.TesiraTextProtocol.Parsing
 		/// <typeparam name="T"></typeparam>
 		/// <param name="objectMap">Maps the TTP string representation to a C# object.</param>
 		/// <returns></returns>
-		[NotNull]
+		[Obsolete("Pass an additional default value")]
 		public T GetObjectValue<T>(IDictionary<string, T> objectMap)
 		{
 			if (objectMap == null)
@@ -231,6 +230,21 @@ namespace ICD.Connect.Audio.Biamp.TesiraTextProtocol.Parsing
 				                      }
 				                      return objectMap[m_Value];
 			                      });
+		}
+
+		/// <summary>
+		/// Gets the value as an object.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="objectMap">Maps the TTP string representation to a C# object.</param>
+		/// <param name="defaultValue"></param>
+		/// <returns></returns>
+		public T GetObjectValue<T>(IDictionary<string, T> objectMap, T defaultValue)
+		{
+			if (objectMap == null)
+				throw new ArgumentNullException("objectMap");
+
+			return GetObjectValue(arg => objectMap.ContainsKey(m_Value) ? objectMap[m_Value] : defaultValue);
 		}
 
 		/// <summary>
