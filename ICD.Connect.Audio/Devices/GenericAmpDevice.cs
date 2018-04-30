b9154827e2a08ef051d83e02ac9ae59af15f2bb5
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-30 16:41:01 -0400

    feat: Initial commit of GenericAmpDevice

---
+++ ICD.Connect.Audio/Devices/GenericAmpDevice.cs
@@ -0,0 +1,31 @@
+﻿using ICD.Connect.Devices;
+
+namespace ICD.Connect.Audio.Devices
+{
+	/// <summary>
+	/// The GenericAmpDevice is essentially a mock audio switcher that provides a single
+	/// volume control, representing the nearest, currently routed volume control.
+	/// </summary>
+	public sealed class GenericAmpDevice : AbstractDevice<GenericAmpDeviceSettings>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public GenericAmpDevice()
+		{
+			Controls.Add(new GenericAmpRouteSwitcherControl(this, 0));
+
+			// Needs to be added after the route control
+			Controls.Add(new GenericAmpVolumeControl(this, 1));
+		}
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+			return true;
+		}
+	}
+}
---
+++ ICD.Connect.Audio/Devices/GenericAmpDeviceSettings.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Devices;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Audio.Devices
+{
+	[KrangSettings("GenericAmpDevice", typeof(GenericAmpDevice))]
+	public sealed class GenericAmpDeviceSettings : AbstractDeviceSettings
+	{
+	}
+}
