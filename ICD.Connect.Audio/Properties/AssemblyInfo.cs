e912575cb163c12727e71dcb71e0c87fc57ce1e2
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-30 16:40:28 -0400

    feat: Initial commit of ICD.Connect.Audio project

---
+++ ICD.Connect.Audio/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Audio")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Audio")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyVersion("3.0.1.0")]
+
