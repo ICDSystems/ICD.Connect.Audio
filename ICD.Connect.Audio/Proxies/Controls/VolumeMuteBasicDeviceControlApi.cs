a3071760e842436aa6c8720af666a9829b0c858a
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-03 10:02:07 -0400

    feat: Initial commit of volume control API classes

---
+++ ICD.Connect.Audio/Proxies/Controls/VolumeMuteBasicDeviceControlApi.cs
@@ -0,0 +1,9 @@
+﻿namespace ICD.Connect.Audio.Proxies.Controls
+{
+	public static class VolumeMuteBasicDeviceControlApi
+	{
+		public const string METHOD_VOLUME_MUTE_TOGGLE = "VolumeMuteToggle";
+
+		public const string HELP_METHOD_VOLUME_MUTE_TOGGLE = "Toggles the current mute state.";
+	}
+}
---
+++ ICD.Connect.Audio/Proxies/Controls/VolumeRawLevelDeviceControlApi.cs
@@ -0,0 +1,11 @@
+﻿namespace ICD.Connect.Audio.Proxies.Controls
+{
+	public static class VolumeRawLevelDeviceControlApi
+	{
+		public const string PROPERTY_VOLUME_RAW_MAX_RANGE = "VolumeRawMaxRange";
+		public const string PROPERTY_VOLUME_RAW_MIN_RANGE = "VolumeRawMinRange";
+
+		public const string HELP_PROPERTY_VOLUME_RAW_MAX_RANGE = "Absolute max volume for the control.";
+		public const string HELP_PROPERTY_VOLUME_RAW_MIN_RANGE = "Absolute min volume for the control.";
+	}
+}
