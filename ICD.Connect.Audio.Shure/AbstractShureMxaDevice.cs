023ec2fbf4e7f446e57ec0208db92ce37846646b
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-07 14:27:39 -0400

    refactor: Removing unused using directive

--- ICD.Connect.Audio.Shure/AbstractShureMxaDevice.cs
+++ ICD.Connect.Audio.Shure/AbstractShureMxaDevice.cs
@@ -3,7 +3,6 @@ using ICD.Common.Utils;
 using ICD.Common.Utils.Services.Logging;
 using ICD.Connect.API.Commands;
 using ICD.Connect.Devices;
-using ICD.Connect.Devices.Controls;
 using ICD.Connect.Devices.EventArguments;
 using ICD.Connect.Protocol.Extensions;
 using ICD.Connect.Protocol.Ports;
