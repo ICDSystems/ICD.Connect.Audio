56370468fdf8f21c400f58279936ee44106696c0
Drew Tingen <drew.tingen@icdsystems.com>
2018-10-10 09:36:47 -0400

    chore: Fix assembly version and copyright info

--- ICD.Connect.Audio.Misc/Properties/AssemblyInfo.cs
+++ ICD.Connect.Audio.Misc/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Audio.Misc")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Audio.Misc")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
