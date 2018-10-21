25c1d342052bce11d8439a69597d8de8dc69df3c
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-21 19:40:38 -0400

    feat: DenonAvr volume control exposes mute console features

--- ICD.Connect.Audio.Denon/Controls/DenonAvrVolumeControl.cs
+++ ICD.Connect.Audio.Denon/Controls/DenonAvrVolumeControl.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using ICD.Common.Utils;
 using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
 using ICD.Common.Utils.Services.Logging;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Audio.Console;
 using ICD.Connect.Audio.Controls;
 using ICD.Connect.Audio.Denon.Devices;
 
@@ -218,6 +222,79 @@ namespace ICD.Connect.Audio.Denon.Controls
 
 			Parent.SendData(DenonSerialData.Request(MASTER_VOLUME));
 			Parent.SendData(DenonSerialData.Request(MUTE));
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleNodeBase> GetConsoleNodes()
+		{
+			foreach (IConsoleNodeBase node in GetBaseConsoleNodes())
+				yield return node;
+
+			foreach (IConsoleNodeBase node in VolumeMuteBasicDeviceControlConsole.GetConsoleNodes(this))
+				yield return node;
+
+			foreach (IConsoleNodeBase node in VolumeMuteDeviceControlConsole.GetConsoleNodes(this))
+				yield return node;
+
+			foreach (IConsoleNodeBase node in VolumeMuteFeedbackDeviceControlConsole.GetConsoleNodes(this))
+				yield return node;
+		}
+
+		/// <summary>
+		/// Wrokaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleNodeBase> GetBaseConsoleNodes()
+		{
+			return base.GetConsoleNodes();
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			VolumeMuteBasicDeviceControlConsole.BuildConsoleStatus(this, addRow);
+			VolumeMuteDeviceControlConsole.BuildConsoleStatus(this, addRow);
+			VolumeMuteFeedbackDeviceControlConsole.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			foreach (IConsoleCommand command in VolumeMuteBasicDeviceControlConsole.GetConsoleCommands(this))
+				yield return command;
+
+			foreach (IConsoleCommand command in VolumeMuteDeviceControlConsole.GetConsoleCommands(this))
+				yield return command;
+
+			foreach (IConsoleCommand command in VolumeMuteFeedbackDeviceControlConsole.GetConsoleCommands(this))
+				yield return command;
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
 		}
 
 		#endregion
