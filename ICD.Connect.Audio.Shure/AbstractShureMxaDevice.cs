6a8267881bdaa764cd6f8d6f74124f37d9282393
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-25 11:49:28 -0400

    fix: Fail gracefully if a port is not available

--- ICD.Connect.Audio.Shure/AbstractShureMxaDevice.cs
+++ ICD.Connect.Audio.Shure/AbstractShureMxaDevice.cs
@@ -322,9 +322,14 @@ namespace ICD.Connect.Audio.Shure
 
 			if (settings.Port != null)
 			{
-				port = factory.GetPortById((int)settings.Port) as ISerialPort;
-				if (port == null)
+				try
+				{
+					port = factory.GetPortById((int)settings.Port) as ISerialPort;
+				}
+				catch (KeyNotFoundException)
+				{
 					Log(eSeverity.Error, "No Serial Port with id {0}", settings.Port);
+				}
 			}
 
 			m_ConnectionStateManager.SetPort(port);
