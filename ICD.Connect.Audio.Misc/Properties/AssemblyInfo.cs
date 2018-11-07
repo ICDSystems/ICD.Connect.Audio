1a0f3b177b365bc3cbd2e7d8938a221172fc65c6
Drew Tingen <drew.tingen@icdsystems.com>
2018-11-07 12:06:09 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Audio.Misc/Properties/AssemblyInfo.cs
+++ ICD.Connect.Audio.Misc/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Audio.Misc")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
 
