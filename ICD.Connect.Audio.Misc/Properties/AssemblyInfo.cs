20fa73edcb9b042687f64fa216c1f8f186256ceb
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-08 16:29:14 -0500

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Audio.Misc/Properties/AssemblyInfo.cs
+++ ICD.Connect.Audio.Misc/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Audio.Misc")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Audio.Misc")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyVersion("8.1.0.0")]
 
