0cbd762be1ee1c4a63fe30abd225753489fde36a
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-02-21 10:51:14 -0500

    Telemetry saftey check in, begin adding features for switcher telemetry

--- ICD.Connect.Audio/Devices/GenericAmpRouteSwitcherControl.cs
+++ ICD.Connect.Audio/Devices/GenericAmpRouteSwitcherControl.cs
@@ -107,6 +107,38 @@ namespace ICD.Connect.Audio.Devices
 			return m_Cache.SetInputForOutput(output, null, type);
 		}
 
+		public override IEnumerable<string> GetSwitcherVideoInputIds()
+		{
+			yield break;
+		}
+
+		/// <summary>
+		/// Gets the Input Name of the switcher (ie Content, Display In)
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<string> GetSwitcherVideoInputNames()
+		{
+			yield break;
+		}
+
+		/// <summary>
+		/// Gets the Input Sync Type of the switcher's inputs (ie HDMI when HDMI Sync is detected, empty when not detected)
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<string> GetSwitcherVideoInputSyncType()
+		{
+			yield break;
+		}
+
+		/// <summary>
+		/// Gets the Input Resolution for the switcher's inputs (ie 1920x1080, or empty for no sync)
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<string> GetSwitcherVideoInputResolution()
+		{
+			yield break;
+		}
+
 		/// <summary>
 		/// Gets the output at the given address.
 		/// </summary>
