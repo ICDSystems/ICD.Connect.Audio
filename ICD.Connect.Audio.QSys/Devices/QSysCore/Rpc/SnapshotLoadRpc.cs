f2a717bcb749d859c8b0d063ea3a11871d81a954
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-18 12:46:48 -0400

    feat: Initial commit of SnapshotSaveRpc

--- ICD.Connect.Audio.QSys/Devices/QSysCore/Rpc/SnapshotLoadRpc.cs
+++ ICD.Connect.Audio.QSys/Devices/QSysCore/Rpc/SnapshotLoadRpc.cs
@@ -9,25 +9,19 @@ namespace ICD.Connect.Audio.QSys.Devices.QSysCore.Rpc
 	///		"id":"3002",
 	///		"method":"Snapshot.Load",
 	///		"params":{
-	///			"Name":"",
-	///			"Bank":0,
+	///			"Name":"Camera1 Presets",
+	///			"Bank":1,
 	///			"Ramp":5
 	///		}
 	/// }
 	/// </summary>
-	public sealed class SnapshotLoadRpc : AbstractRpc
+	public sealed class SnapshotLoadRpc : AbstractSnapshotRpc
 	{
-		private const string NAME_PROPERTY = "Name";
-		private const string BANK_PROPERTY = "Bank";
 		private const string RAMP_PROPERTY = "Ramp";
 
 		private const string METHOD_VALUE = "Snapshot.Load";
 
 		public override string Method { get { return METHOD_VALUE; } }
-
-		public string Name { get; set; }
-
-		public int Bank { get; set; }
 
 		public int? Ramp { get; set; }
 
@@ -37,8 +31,8 @@ namespace ICD.Connect.Audio.QSys.Devices.QSysCore.Rpc
 		/// <param name="writer"></param>
 		protected override void SerializeParams(JsonWriter writer)
 		{
-			writer.WriteProperty(NAME_PROPERTY, Name);
-			writer.WriteProperty(BANK_PROPERTY, Bank);
+			base.SerializeParams(writer);
+
 			writer.WriteProperty(RAMP_PROPERTY, Ramp);
 		}
 	}
