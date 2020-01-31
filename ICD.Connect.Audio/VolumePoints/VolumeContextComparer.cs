b708f26ef67789de46a56b69c74742cc363d13a2
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-31 11:58:08 -0500

    feat: Added VolumeContextComparer

---
+++ ICD.Connect.Audio/VolumePoints/VolumeContextComparer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ICD.Common.Utils;
+using ICD.Common.Utils.Extensions;
+
+namespace ICD.Connect.Audio.VolumePoints
+{
+	/// <summary>
+	/// Comparer which determines the greater volume point based on contextual priority using flags.
+	/// </summary>
+	public sealed class VolumeContextComparer : IComparer<IVolumePoint>
+	{
+		private readonly eVolumeType m_Context;
+
+		public VolumeContextComparer(eVolumeType context)
+		{
+			m_Context = context;
+		}
+
+		public int Compare(IVolumePoint x, IVolumePoint y)
+		{
+			foreach (eVolumeType flag in EnumUtils.GetFlagsExceptNone(m_Context))
+			{
+				if (x.VolumeType.HasFlag(flag))
+					return 1;
+
+				if (y.VolumeType.HasFlag(flag))
+					return 1;
+			}
+
+			return 0;
+		}
+	}
+}
