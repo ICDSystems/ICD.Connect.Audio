8c58d5d3281726da3b5fa333903ba30275603fce
Drew Tingen <drew.tingen@icdsystems.com>
2020-02-02 20:06:29 -0500

    feat: Adding BiColorRelayMicButton

---
+++ ICD.Connect.Audio.Misc/BiColorMicButton/BiColorRelayMicButtonDeviceSettings.cs
@@ -0,0 +1,51 @@
+﻿using ICD.Common.Utils.Xml;
+using ICD.Connect.Protocol.Ports.RelayPort;
+using ICD.Connect.Settings.Attributes;
+using ICD.Connect.Settings.Attributes.SettingsProperties;
+
+namespace ICD.Connect.Audio.Misc.BiColorMicButton
+{
+	[KrangSettings("BiColorRelayMicButton", typeof(BiColorRelayMicButtonDevice))]
+	public sealed class BiColorRelayMicButtonDeviceSettings : AbstractBiColorMicButtonDeviceSettings
+	{
+		private const string POWER_OUTPUT_PORT_ELEMENT = "PowerOutputPort";
+		private const string RED_LED_OUTPUT_PORT_ELEMENT = "RedLedOutputPort";
+		private const string GREEN_LED_OUTPUT_PORT_ELEMENT = "GreenLedOutputPort";
+
+		[OriginatorIdSettingsProperty(typeof(IRelayPort))]
+		public int? PowerOutputPort { get; set; }
+
+		[OriginatorIdSettingsProperty(typeof(IRelayPort))]
+		public int? RedLedOutputPort { get; set; }
+
+		[OriginatorIdSettingsProperty(typeof(IRelayPort))]
+		public int? GreenLedOutputPort { get; set; }
+
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(POWER_OUTPUT_PORT_ELEMENT, IcdXmlConvert.ToString(PowerOutputPort));
+			writer.WriteElementString(RED_LED_OUTPUT_PORT_ELEMENT, IcdXmlConvert.ToString(RedLedOutputPort));
+			writer.WriteElementString(GREEN_LED_OUTPUT_PORT_ELEMENT, IcdXmlConvert.ToString(GreenLedOutputPort));
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			PowerOutputPort = XmlUtils.TryReadChildElementContentAsInt(xml, POWER_OUTPUT_PORT_ELEMENT);
+			RedLedOutputPort = XmlUtils.TryReadChildElementContentAsInt(xml, RED_LED_OUTPUT_PORT_ELEMENT);
+			GreenLedOutputPort = XmlUtils.TryReadChildElementContentAsInt(xml, GREEN_LED_OUTPUT_PORT_ELEMENT);
+		}
+	}
+}
