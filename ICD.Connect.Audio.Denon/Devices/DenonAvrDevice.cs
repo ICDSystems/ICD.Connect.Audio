9b4c900942e80204cbed715e365d1e5727d0e3c5
Chris Cameron <chris.cameron@wedoresi.com>
2020-04-24 16:12:07 -0400

    fix: ConnectStateManager port configuration callback changed

--- ICD.Connect.Audio.Denon/Devices/DenonAvrDevice.cs
+++ ICD.Connect.Audio.Denon/Devices/DenonAvrDevice.cs
@@ -158,7 +158,7 @@ namespace ICD.Connect.Audio.Denon.Devices
 		/// Configures the given port for communication with the device.
 		/// </summary>
 		/// <param name="port"></param>
-		private void ConfigurePort(ISerialPort port)
+		private void ConfigurePort(IPort port)
 		{
 			// Com
 			if (port is IComPort)
--- ICD.Connect.Audio.QSys/Devices/QSysCore/QSysCoreDevice.cs
+++ ICD.Connect.Audio.QSys/Devices/QSysCore/QSysCoreDevice.cs
@@ -148,7 +148,7 @@ namespace ICD.Connect.Audio.QSys.Devices.QSysCore
 		/// Configures the given port for communication with the device.
 		/// </summary>
 		/// <param name="port"></param>
-		private void ConfigurePort(ISerialPort port)
+		private void ConfigurePort(IPort port)
 		{
 			// Network (TCP, UDP, SSH)
 			if (port is ISecureNetworkPort)
