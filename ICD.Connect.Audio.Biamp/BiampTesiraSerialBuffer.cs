6785ea4f921dc87dddaf34a8b4ed356b881c58fb
Chris Cameron <chris.cameron@icdsystems.com>
2020-05-22 23:34:20 -0400

    refactor: Using AbstractSerialBuffer

--- ICD.Connect.Audio.Biamp/BiampTesiraSerialBuffer.cs
+++ ICD.Connect.Audio.Biamp/BiampTesiraSerialBuffer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using ICD.Common.Utils.EventArguments;
-using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Audio.Biamp.TesiraTextProtocol.Parsing;
 using ICD.Connect.Protocol.Network.Ports.Tcp;
@@ -9,126 +8,83 @@ using ICD.Connect.Protocol.SerialBuffers;
 
 namespace ICD.Connect.Audio.Biamp
 {
-	public sealed class BiampTesiraSerialBuffer : ISerialBuffer
+	public sealed class BiampTesiraSerialBuffer : AbstractSerialBuffer
 	{
-		public const string WELCOME_TEXT = "Welcome to the Tesira Text Protocol Server...";
+		private const string WELCOME_TEXT = "Welcome to the Tesira Text Protocol Server...";
 		private const string PUBLISH_RESPONSE = "! \"publishToken\":";
 
-		public event EventHandler<StringEventArgs> OnCompletedSerial;
-		public event EventHandler<StringEventArgs> OnSerialTelnetHeader;
-		public event EventHandler<StringEventArgs> OnSubscribeResponse;
-		public event EventHandler OnWelcomeMessageReceived;
-
-		private string m_Remainder;
-		private readonly Queue<string> m_Queue;
-
-		private readonly SafeCriticalSection m_QueueSection;
-		private readonly SafeCriticalSection m_ParseSection;
-
-		private readonly char[] m_Delimiters =
+		private static readonly char[] s_Delimiters =
 		{
 			TtpUtils.CR,
 			TtpUtils.LF
 		};
 
 		/// <summary>
-		/// Constructor.
+		/// Raised when a telnet header is discovered.
 		/// </summary>
-		public BiampTesiraSerialBuffer()
-		{
-			m_Queue = new Queue<string>();
+		public event EventHandler<StringEventArgs> OnSerialTelnetHeader;
 
-			m_QueueSection = new SafeCriticalSection();
-			m_ParseSection = new SafeCriticalSection();
+		/// <summary>
+		/// Raised when a subscription response message is discovered.
+		/// </summary>
+		public event EventHandler<StringEventArgs> OnSubscribeResponse;
+
+		/// <summary>
+		/// Raised when the welcome message is discovered.
+		/// </summary>
+		public event EventHandler OnWelcomeMessageReceived;
+
+		private string m_Remainder;
+
+		/// <summary>
+		/// Override to clear any current state.
+		/// </summary>
+		protected override void ClearFinal()
+		{
+			m_Remainder = null;
 		}
 
 		/// <summary>
-		/// Enqueues the serial data.
+		/// Override to process the given item for chunking.
 		/// </summary>
 		/// <param name="data"></param>
-		public void Enqueue(string data)
+		protected override IEnumerable<string> Process(string data)
 		{
-			m_QueueSection.Execute(() => m_Queue.Enqueue(data));
-			Parse();
-		}
+			// Prepend anything left from the previous pass
+			m_Remainder = (m_Remainder ?? string.Empty) + data;
+			if (m_Remainder.Length == 0)
+				yield break;
 
-		/// <summary>
-		/// Clears all queued data in the buffer.
-		/// </summary>
-		public void Clear()
-		{
-			m_ParseSection.Enter();
-			m_QueueSection.Enter();
-
-			try
+			// First check for telnet negotiation
+			while (m_Remainder.Length >= 3 && m_Remainder[0] == TelnetControl.HEADER)
 			{
-				m_Remainder = null;
-				m_Queue.Clear();
+				string output = m_Remainder.Substring(0, 3);
+				m_Remainder = m_Remainder.Substring(3);
+				OnSerialTelnetHeader.Raise(this, new StringEventArgs(output));
 			}
-			finally
-			{
-				m_ParseSection.Leave();
-				m_QueueSection.Leave();
-			}
-		}
 
-		/// <summary>
-		/// Searches the enqueued serial data for the delimiter character.
-		/// Complete strings are raised via the OnCompletedString event.
-		/// </summary>
-		private void Parse()
-		{
-			if (!m_ParseSection.TryEnter())
-				return;
-
-			try
+			// Look for delimiters
+			while (m_Remainder.Length > 0)
 			{
-				string data = null;
-				
-				while (m_QueueSection.Execute(() => m_Queue.Dequeue(out data)))
+				int index = m_Remainder.IndexOfAny(s_Delimiters);
+				if (index < 0)
+					break;
+
+				string output = m_Remainder.Substring(0, index);
+				m_Remainder = m_Remainder.Substring(index + 1);
+
+				if (string.IsNullOrEmpty(output))
+					continue;
+
+				if (output == WELCOME_TEXT)
 				{
-					// Prepend anything left from the previous pass
-					m_Remainder = (m_Remainder ?? string.Empty) + data;
-					if (m_Remainder.Length == 0)
-						continue;
-
-					// First check for telnet negotiation
-					while (m_Remainder.Length >= 3 && m_Remainder[0] == TelnetControl.HEADER)
-					{
-						string output = m_Remainder.Substring(0, 3);
-						m_Remainder = m_Remainder.Substring(3);
-						OnSerialTelnetHeader.Raise(this, new StringEventArgs(output));
-					}
-
-					// Look for delimiters
-					while (m_Remainder.Length > 0)
-					{
-						int index = m_Remainder.IndexOfAny(m_Delimiters);
-						if (index < 0)
-							break;
-
-						string output = m_Remainder.Substring(0, index);
-						m_Remainder = m_Remainder.Substring(index + 1);
-
-						if (output == WELCOME_TEXT)
-						{
-							OnWelcomeMessageReceived.Raise(this);
-							continue;
-						}
-
-						if (!string.IsNullOrEmpty(output))
-						{
-							if (output.StartsWith(PUBLISH_RESPONSE))
-								OnSubscribeResponse.Raise(this, new StringEventArgs(output));
-							else
-								OnCompletedSerial.Raise(this, new StringEventArgs(output));
-						}
-					}
+					OnWelcomeMessageReceived.Raise(this);
+					continue;
 				}
-			}
-			finally
-			{
-				m_ParseSection.Leave();
+
+				if (output.StartsWith(PUBLISH_RESPONSE))
+					OnSubscribeResponse.Raise(this, new StringEventArgs(output));
+				yield return output;
 			}
 		}
 	}
--- ICD.Connect.Audio.Shure/ShureMicSerialBuffer.cs
+++ ICD.Connect.Audio.Shure/ShureMicSerialBuffer.cs
@@ -1,30 +1,19 @@
-using System;
 using System.Collections.Generic;
 using System.Text;
-using ICD.Common.Utils;
-using ICD.Common.Utils.EventArguments;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Protocol.SerialBuffers;
 
 namespace ICD.Connect.Audio.Shure
 {
-	public sealed class ShureMicSerialBuffer : ISerialBuffer
+	public sealed class ShureMicSerialBuffer : AbstractSerialBuffer
 	{
-		public event EventHandler<StringEventArgs> OnCompletedSerial;
-
-		private readonly StringBuilder m_RxData;
-		private readonly Queue<string> m_Queue;
-
-		private readonly SafeCriticalSection m_QueueSection;
-		private readonly SafeCriticalSection m_ParseSection;
-
 		private static readonly char[] s_Delimiters =
 		{
 			'<',
 			'>'
 		};
 
-		#region Constructors
+		private readonly StringBuilder m_RxData;
 
 		/// <summary>
 		/// Constructor.
@@ -32,102 +21,55 @@ namespace ICD.Connect.Audio.Shure
 		public ShureMicSerialBuffer()
 		{
 			m_RxData = new StringBuilder();
-			m_Queue = new Queue<string>();
-
-			m_QueueSection = new SafeCriticalSection();
-			m_ParseSection = new SafeCriticalSection();
 		}
 
-		#endregion
-
-		#region Methods
+		/// <summary>
+		/// Override to clear any current state.
+		/// </summary>
+		protected override void ClearFinal()
+		{
+			m_RxData.Clear();
+		}
 
 		/// <summary>
-		/// Enqueues the serial data.
+		/// Override to process the given item for chunking.
 		/// </summary>
 		/// <param name="data"></param>
-		public void Enqueue(string data)
+		/// <returns></returns>
+		protected override IEnumerable<string> Process(string data)
 		{
-			m_QueueSection.Execute(() => m_Queue.Enqueue(data));
-			Parse();
-		}
-
-		/// <summary>
-		/// Clears all queued data in the buffer.
-		/// </summary>
-		public void Clear()
-		{
-			m_ParseSection.Enter();
-			m_QueueSection.Enter();
-
-			try
+			while (true)
 			{
-				m_RxData.Clear();
-				m_Queue.Clear();
-			}
-			finally
-			{
-				m_ParseSection.Leave();
-				m_QueueSection.Leave();
-			}
-		}
+				int index = data.IndexOfAny(s_Delimiters);
 
-		#endregion
-
-		/// <summary>
-		/// Searches the enqueued serial data for the delimiter character.
-		/// Complete strings are raised via the OnCompletedString event.
-		/// </summary>
-		private void Parse()
-		{
-			if (!m_ParseSection.TryEnter())
-				return;
-
-			try
-			{
-				string data = null;
-
-				while (m_QueueSection.Execute(() => m_Queue.Dequeue(out data)))
+				// Simple case - No delimiters in the data
+				if (index < 0)
 				{
-					while (true)
-					{
-						int index = data.IndexOfAny(s_Delimiters);
-
-						// Simple case - No delimiters in the data
-						if (index < 0)
-						{
-							if (m_RxData.Length > 0)
-								m_RxData.Append(data);
-							break;
-						}
-
-						char token = data[index];
-
-						switch (token)
-						{
-							case '<':
-								// Trim any leading nonsense
-								m_RxData.Clear();
-								m_RxData.Append('<');
-								break;
-
-							case '>':
-								if (m_RxData.Length > 0)
-								{
-									m_RxData.Append(data.Substring(0, index + 1));
-									string output = m_RxData.Pop();
-									OnCompletedSerial.Raise(this, new StringEventArgs(output));
-								}
-								break;
-						}	
-
-						data = data.Substring(index + 1);
-					}
+					if (m_RxData.Length > 0)
+						m_RxData.Append(data);
+					break;
 				}
-			}
-			finally
-			{
-				m_ParseSection.Leave();
+
+				char token = data[index];
+
+				switch (token)
+				{
+					case '<':
+						// Trim any leading nonsense
+						m_RxData.Clear();
+						m_RxData.Append('<');
+						break;
+
+					case '>':
+						if (m_RxData.Length > 0)
+						{
+							m_RxData.Append(data.Substring(0, index + 1));
+							yield return m_RxData.Pop();
+						}
+						break;
+				}
+
+				data = data.Substring(index + 1);
 			}
 		}
 	}
